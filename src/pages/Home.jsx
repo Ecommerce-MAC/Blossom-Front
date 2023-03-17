a4dc2efce50351480a6f68da788223797d0af145
acalabustos <acalabustos@gmail.com>
2023-03-17 12:44:11 +0100

    base

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+
+
+function Home() {
+    return (
+        <>
+            <Container fluid>
+                <Navbar></Navbar>
+                <Carrousel>
+            
+                </Carrousel>
+                
+                <Footer></Footer>
+            </Container>
+
+        </>
+
+    )
+}
+export default Home
