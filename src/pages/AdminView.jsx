957356880751481aec55a9a4aa97e19b29c42408
MariaGF <miriangimenez@gmail.com>
2023-03-20 14:03:26 +0100

    added AdminView

--- src/pages/AdminView.jsx
+++ src/pages/AdminView.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import '../Styles/AdminView.css'
 
 
 function Admin() {
+return (
+<h1 className=" text-3xl">Hello, "Admin"</h1>
 
+);
          
-}
+};
 
 
 export default Admin;
