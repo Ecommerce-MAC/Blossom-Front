166f44841db1b481dcbb7aa68bb1bef0d620a8bc
acalabustos <acalabustos@gmail.com>
2023-03-30 17:40:08 +0200

    merge popup

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -43,15 +43,10 @@ export default function Navbar ()  {
 
         <Fragment>
         <div>
-<<<<<<< HEAD
-           <button className= "text-black bg-blue-70" onClick={() => setShowModal(true)}>
-            Login
-=======
            <button className= "text-dark bg-blue-700 hover:bg-pink" onClick={() => setShowModal(true)}>
               <span className="material-symbols-outlined">
                 account_circle
               </span>
->>>>>>> dev
            </button>
         </div>
         <Modal isVisible={showModal} onClose={() => setShowModal(false)}>         
@@ -59,11 +54,6 @@ export default function Navbar ()  {
         </Modal>
         </Fragment>
         <button className="text-green-500 bg-blue-700 hover:bg-pink-500 px-4 py-2 rounded" onClick={() => window.location.href='/CartView'}>
-<<<<<<< HEAD
-        Cart
-      </button>
-      
-=======
             <span className="material-symbols-outlined">
              shopping_cart
             </span>
@@ -71,7 +61,6 @@ export default function Navbar ()  {
       <button className=".material-symbols-outlined text-green-500 bg-blue-700 hover:bg-pink-500 px-4 py-2 rounded" onClick={() => window.location.href='/UploadProduct'}>
         Upload
       </button>
->>>>>>> dev
       <button className="text-green-500 bg-blue-700 hover:bg-pink-500 px-4 py-2 rounded" onClick={() => window.location.href='/AdminView'}>
         Admin
       </button>
