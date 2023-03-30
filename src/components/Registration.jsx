52d6024462affd801ad44e93a15c1cc7f8034be1
acalabustos <acalabustos@gmail.com>
2023-03-30 18:37:19 +0200

    style

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -6,13 +6,6 @@ import { useState } from "react";
 export default function Registration() {
   const [showModal, setShowModal] = useState(false); 
 
-  // const handleForgotPassword = () => {
-  //   setShowModal(true); 
-  // };
-
-  // const handleCloseModal = () => {
-  //   setShowModal(false); 
-  // };
   return (
     <>
       <div className="flex min-h-full items-center justify-center  py-12 px-4 sm:px-6 lg:px-8">
@@ -35,40 +28,19 @@ export default function Registration() {
                 <label htmlFor="email-address" className="sr-only">
                   Email address
                 </label>
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="relative block w-full rounded-t-md  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="name@company.com"
-                />
+                <input className="relative block w-full rounded-t-md  py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder=" name@company.com"/>
               </div>
               <div>
                 <label htmlFor="password" className="sr-only">
                   Password
                 </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="*******"
-                />
+                <input className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder=" *******"/>
               </div>
             </div>
 
             <div className="flex items-center justify-between">
               <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                />
+                <input className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
                 <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
                   Remember me
                 </label>
@@ -88,9 +60,9 @@ export default function Registration() {
             </div>
 
             <div>
-              <button type="submit"className="w-full text-dark bg-blue-700 hover:bg-blue-800 focus:ring-4 focus: outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text center">
+              <button type="submit"className="mt-6 w-full rounded-md bg-blue-500 bg-orange py-1.5 font-medium text-blue-50 hover:bg-blue-600">
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                  
+              
                 </span> 
                 Sign in
               </button>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,36 +2,24 @@
 
 export default function Sidebar() {
 
-
-
-
     return (
 
-
-        <div class="relative bg-gray-50 dark:bg-slate-900 w-screen h-screen pattern">
-            <nav class="z-10 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg dark:border-slate-600/60 dark:bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-4 min-h-[auto] min-w-[4px] flex-col rounded-lg border">
-                <a href='/UploadProduct' class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
-
-                    <small class="text-center text-xs font-medium">Upload Product</small>
+        <div>
+            <nav className="z-10 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg dark:border-slate-600/60 dark:bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-4 min-h-[auto] min-w-[4px] flex-col rounded-lg border">
+                <a href='/UploadProduct' className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
+                    <small className="text-center text-xs font-medium">Upload Product</small>
                 </a>
-                <a href='/AdminView/Cosmetics' class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap- rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
-
-
-                    <small class="text-center text-xs font-medium">Cosmetics </small>
+                <a href='/AdminView/Cosmetics' className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap- rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
+                    <small className="text-center text-xs font-medium">Cosmetics </small>
                 </a>
-                <a href='/AdminView/Soap' class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
-
-                    <small class="text-center text-xs font-medium"> Soaps </small>
+                <a href='/AdminView/Soap' className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
+                    <small className="text-center text-xs font-medium"> Soaps </small>
                 </a>
-                <a href='/AdminView/Aromatherapy' class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
-
-                    <small class="text-center text-xs font-medium"> Aromatherapy </small>
+                <a href='/AdminView/Aromatherapy' className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
+                    <small className="text-center text-xs font-medium"> Aromatherapy </small>
                 </a>
-                <a href='/AdminView/Hair' class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
-
-                    
-
-                    <small class="text-center text-xs font-medium"> Hair </small>
+                <a href='/AdminView/Hair' className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50">
+                    <small className="text-center text-xs font-medium"> Hair </small>
                 </a>
 
             </nav>
